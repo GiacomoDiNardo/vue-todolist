d36c401fc66c81af58106ce8294058cdd96d14d2
GiacomoDiNardo <vest_95@hotmail.it>
2022-06-13 16:10:03 +0200

    created basic layout, create to do list

---
+++ main.js
@@ -0,0 +1,27 @@
+new Vue({
+    el: "#app",
+
+    data: {
+        listToDo: [
+            {
+                text: "Pulire camera",
+                done: false,
+            },
+            {
+                text: "Appendere staffe",
+                done: false,
+            },
+            {
+                text: "Tagliare prato",
+                done: false,
+            },
+        ]
+    },
+
+    methods : {
+        itsDone: function(index) {
+            this.listToDo[index].done === true;
+        }
+    }
+
+})
